c7257d292212051a450833c063d854c2f5438670
HelloKitty <andrew.blakely@ymail.com>
2016-12-20 16:57:51 -0600

    Remove BaseType auth payload serialization
    This is not possible. There is not support for 1 to n or n to 1. Only 1
    to 1 relationships with keys/opcodes and payloads types.

--- src/FreecraftCore.Packet.Auth/Payloads/IAuthenticationPayload.cs
+++ src/FreecraftCore.Packet.Auth/Payloads/IAuthenticationPayload.cs
@@ -7,8 +7,10 @@ using System.Threading.Tasks;
 
 namespace FreecraftCore.Packet.Auth
 {
-	[WireMessage]
-	[WireMessageBaseType(1, typeof(AuthLogonChallengeRequest))]
+	//We cannot use basetype polymorphic serialization. The serializer supports only 1 to 1 for keys.
+	//In authentication's case there is a response and a request associated with the command/opcode. Therefore it cannot be used.
+	//[WireMessage]
+	//[WireMessageBaseType(1, typeof(AuthLogonChallengeRequest))]
 	public interface IAuthenticationPayload //this is sorta like a Java metadata interface marker. We use it for polymorphic serialization
 	{
 
