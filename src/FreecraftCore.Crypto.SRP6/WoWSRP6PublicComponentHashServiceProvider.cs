f66d838cc3a3eb13ce1fe67f0e5845756ce04eb7
HelloKitty <andrew.blakely@ymail.com>
2016-12-21 19:37:06 -0600

    Added Mangos' session key hashing

--- src/FreecraftCore.Crypto.SRP6/WoWSRP6PublicComponentHashServiceProvider.cs
+++ src/FreecraftCore.Crypto.SRP6/WoWSRP6PublicComponentHashServiceProvider.cs
@@ -28,6 +28,51 @@ namespace FreecraftCore.Crypto.SRP6
 			}
 		}
 
+		public BigInteger HashSessionKey(BigInteger key)
+		{
+			//See: https://github.com/vermie/MangosClient/blob/master/Client/Authentication/Network/AuthSocket.cs for information
+			//I don't know what hashing algorithm this is but SRP6 calls for
+			//hashing the session key with a one-way hashing algorithm
+			//Maybe this is a custom Blizzrd hashing algorithm?
+
+			byte[] keyHash;
+			byte[] sData = key.ToCleanByteArray();
+			if (sData.Length < 32)
+			{
+				var tmpBuffer = new byte[32];
+				Buffer.BlockCopy(sData, 0, tmpBuffer, 32 - sData.Length, sData.Length);
+				sData = tmpBuffer;
+			}
+			byte[] keyData = new byte[40];
+			byte[] temp = new byte[16];
+
+			// take every even indices byte, hash, store in even indices
+			for (int i = 0; i < 16; ++i)
+				temp[i] = sData[i * 2];
+
+			using (SHA1 shaProvider = SHA1.Create())
+			{
+				keyHash = shaProvider.ComputeHash(temp);
+			}
+
+			for (int i = 0; i < 20; ++i)
+				keyData[i * 2] = keyHash[i];
+
+			// do the same for odd indices
+			for (int i = 0; i < 16; ++i)
+				temp[i] = sData[i * 2 + 1];
+
+			using (SHA1 shaProvider = SHA1.Create())
+			{
+				keyHash = shaProvider.ComputeHash(temp);
+			}
+
+			for (int i = 0; i < 20; ++i)
+				keyData[i * 2 + 1] = keyHash[i];
+
+			return new BigInteger(keyData);
+		}
+
 		#region IDisposable Support
 		private bool disposedValue = false; // To detect redundant calls
 
