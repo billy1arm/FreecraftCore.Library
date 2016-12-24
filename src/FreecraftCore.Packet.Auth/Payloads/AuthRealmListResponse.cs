c47b89c5e1fd434083cefa4c66b5e913a0f05ddc
HelloKitty <andrew.blakely@ymail.com>
2016-12-23 18:22:21 -0600

    Fix Incorrect RealmList packet structure

--- src/FreecraftCore.Packet.Auth/Payloads/AuthRealmListResponse.cs
+++ src/FreecraftCore.Packet.Auth/Payloads/AuthRealmListResponse.cs
@@ -14,16 +14,22 @@ namespace FreecraftCore.Packet.Auth
 	[AuthenticationPayload(Common.AuthOperationCode.REALM_LIST)]
 	public class AuthRealmListResponse : IAuthenticationPayload
 	{
+		/// <summary>
+		/// The size of the payload.
+		/// </summary>
+		[WireMember(1)]
+		private ushort payloadSize { get; set; }
+
 		//Unknown field. Trinitycore always sends 0.
 		//I think EmberEmu said it's expected as 0 in the client? Can't recall
-		[WireMember(1)]
+		[WireMember(2)]
 		private int unknownOne { get; set; }
 
 		/// <summary>
 		/// Realm information.
 		/// </summary>
 		[SendSize(SendSizeAttribute.SizeType.UShort)] //in 2.x and 3.x this is ushort but in 1.12.1 it's a uint32
-		[WireMember(2)]
+		[WireMember(3)]
 		private RealmInfo[] realms { get; set; }
 
 		/// <summary>
@@ -34,7 +40,8 @@ namespace FreecraftCore.Packet.Auth
 		//2.x and 3.x clients send byte 16 and 0
 		//1.12.1 clients send 0 and 2.
 		//EmberEmu has no information on what it is.
-		private short unknownValue { get; set; }
+		[WireMember(4)]
+		private short unknownTwo { get; set; }
 
 		public AuthRealmListResponse()
 		{
