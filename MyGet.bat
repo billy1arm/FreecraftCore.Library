81a281c58004a9f0a09ab1c58588c8d5ea5aa26a
HelloKitty <andrew.blakely@ymail.com>
2017-02-09 23:31:27 -0600

    Added Steam and RC4 llibraries to myget build

--- MyGet.bat
+++ MyGet.bat
@@ -1,3 +1,5 @@
 dotnet pack src/FreecraftCore.Crypto.BigInteger/ -c Release
 dotnet pack src/FreecraftCore.Crypto.SRP6/ -c Release
+dotnet pack src/FreecraftCore.Crypto.Stream/ -c Release
+dotnet pack src/FreecraftCore.Crypto.RC4/ -c Release
 
