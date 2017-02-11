73a62d059889f0c9ad170f7918f777f1c559601b
HelloKitty <andrew.blakely@ymail.com>
2017-02-10 18:11:07 -0600

    Added OneTimeCryptoStreamWriter

---
+++ src/FreecraftCore.Crypto.Stream/Writer/OneTimeCryptoStreamWriter.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore.Crypto
+{
+	/// <summary>
+	/// Provides a crypto service implementation of the
+	/// wire writer strategy. Preforms crypto operations only once on the data.
+	/// Must call <see cref="OneTimeCryptoStreamWriter"/>.GetBytes() to encrypt the byte array.
+	/// </summary>
+	public class OneTimeCryptoStreamWriter : CryptoStreamWriter
+	{
+		public enum State
+		{
+			/// <summary>
+			/// Indicates that the writer is currently still writing.
+			/// </summary>
+			Default = 0,
+
+			/// <summary>
+			/// Indicates that the writer has crypted.
+			/// </summary>
+			Crypted = 1,
+		}
+
+		/// <summary>
+		/// Indicates the state of the writer.
+		/// If the writer is <see cref="State.Crypted"/> it can no longer write.
+		/// Calls to GetBytes will force a crypted state.
+		/// </summary>
+		public State WriterState { get; private set; }
+
+		private Lazy<byte[]> CryptoByteRepresentation { get; }
+
+		public OneTimeCryptoStreamWriter([NotNull] IWireStreamWriterStrategy dest, [NotNull] ISessionPacketCryptoService sessionCrypto)
+			: base(dest, sessionCrypto)
+		{
+			//The starting state is default
+			//The writer can only write when it's in default state.
+			WriterState = State.Default;
+
+			//Lazily encrypt the stream's bytes.
+			//This means it can ONLY be done once.
+			CryptoByteRepresentation = new Lazy<byte[]>(() =>
+			{
+				WriterState = State.Crypted;
+				return sessionCrypto.ProcessBytes(dest.GetBytes(), 0);
+			}, true);
+		}
+
+		//See doc for IWireStreamReaderStrategy
+		public override void Write(byte[] data)
+		{
+			ThrowIfEncrypted();
+			Dest.Write(data);
+		}
+
+		public override void Write(byte[] data, int offset, int count)
+		{
+			ThrowIfEncrypted();
+			Dest.Write(data, offset, count);
+		}
+
+		public override void Write(byte data)
+		{
+			ThrowIfEncrypted();
+			Dest.Write(data);
+		}
+
+		public override byte[] GetBytes()
+		{
+			//Once this returns it's crypted and this writer can no longer write
+			return CryptoByteRepresentation.Value;
+		}
+
+		public byte[] Crypt()
+		{
+			return GetBytes();
+		}
+
+		private void ThrowIfEncrypted()
+		{
+			if (WriterState == State.Crypted)
+				throw new InvalidOperationException($"Cannot write bytes to {nameof(OneTimeCryptoStreamWriter)} after the stream has been encrypted.");
+		}
+	}
+}
--- src/FreecraftCore.Crypto.Stream/Writer/RealtimeCryptoStreamWriter.cs
+++ src/FreecraftCore.Crypto.Stream/Writer/RealtimeCryptoStreamWriter.cs
@@ -11,52 +11,35 @@ namespace FreecraftCore.Crypto
 	/// Provides a realtime crypto service implementation of the
 	/// wire writer strategy. Processes the stream chunks at request as opposed to all at once.
 	/// </summary>
-	public class RealtimeCryptoStreamWriter : CryptoStreamService, IWireStreamWriterStrategy, IDisposable
+	public class RealtimeCryptoStreamWriter : CryptoStreamWriter
 	{
-		/// <summary>
-		/// Decorated <see cref="IWireStreamReaderStrategy"/>.
-		/// </summary>
-		[NotNull]
-		private IWireStreamWriterStrategy Dest { get; }
-
 		/// <summary>
 		/// Creates a realtime crypto stream reader.
 		/// </summary>
 		/// <param name="dest">The <see cref="IWireStreamWriterStrategy"/> to decorate.</param>
 		/// <param name="sessionCrypto"></param>
 		public RealtimeCryptoStreamWriter([NotNull] IWireStreamWriterStrategy dest, [NotNull] ISessionPacketCryptoService sessionCrypto)
-			: base(sessionCrypto)
+			: base(dest, sessionCrypto)
 		{
-			//We don't implement the reading ourselves because it could be a default behavior
-			//our we may need network stream reading, which may block, or anything under the sun
-			if (dest == null) throw new ArgumentNullException(nameof(dest));
 
-			Dest = dest;
 		}
 
-		public void Dispose()
-		{
-			//Do not dispose the crypto service
-			//We don't own it
-			Dest.Dispose();
-		}
-
-		public void Write(byte[] data)
+		public override void Write(byte[] data)
 		{
 			Dest.Write(SessionCryptoService.ProcessBytes(data, 0));
 		}
 
-		public void Write(byte[] data, int offset, int count)
+		public override void Write(byte[] data, int offset, int count)
 		{
 			Dest.Write(SessionCryptoService.ProcessBytes(data, offset, count), offset, count);
 		}
 
-		public void Write(byte data)
+		public override void Write(byte data)
 		{
 			Dest.Write(SessionCryptoService.ReturnByte(data));
 		}
 
-		public byte[] GetBytes()
+		public override byte[] GetBytes()
 		{
 			return Dest.GetBytes();
 		}
