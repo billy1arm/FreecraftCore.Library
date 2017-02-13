427b9d488cabc48245455038b7eb56d7249d9038
HelloKitty <andrew.blakely@ymail.com>
2017-02-13 17:26:07 -0600

    Impl SMSG_AUTH_RESPONSE as AuthenticateSessionResponse

---
+++ src/FreecraftCore.Packet.Game.Auth/Packets/AuthenticateSessionResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FreecraftCore.API.Common;
+using FreecraftCore.Packet.Common;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore.Packet
+{
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_AUTH_RESPONSE)] //the response to a request to authenticate a session
+	public class AuthenticateSessionResponse : GamePacketPayload
+	{
+		/// <inheritdoc />
+		public override bool isValid { get; } = true;
+
+		/// <summary>
+		/// Indicates the result of the session authentication.
+		/// </summary>
+		[WireMember(1)]
+		public SessionAuthenticationResult AuthenticationResult { get; private set; }
+
+		public AuthenticateSessionResponse()
+		{
+			//TODO: If we ever run a server add a ctor
+		}
+	}
+}
