23b520744469fd6467bff09057006cda10f3f64a
HelloKitty <andrew.blakely@ymail.com>
2017-02-13 17:36:41 -0600

    Create Warden Packet Project

--- MyGet.bat
+++ MyGet.bat
@@ -3,4 +3,4 @@ dotnet pack src/FreecraftCore.Packet.Auth/ -c Release
 dotnet pack src/FreecraftCore.Packet.Game.Common/ -c Release
 dotnet pack src/FreecraftCore.Packet.Game.Client/ -c Release
 dotnet pack src/FreecraftCore.Packet.Game.Auth/ -c Release
-
+dotnet pack src/FreecraftCore.Packet.Game.Warden/ -c Release
