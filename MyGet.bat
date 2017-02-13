5415de564ff90022532d8152024ae31acb0c7598
HelloKitty <andrew.blakely@ymail.com>
2017-02-13 17:59:53 -0600

    Added Warden Operation Enums

--- MyGet.bat
+++ MyGet.bat
@@ -1,3 +1,3 @@
 dotnet pack src/FreecraftCore.API.Common.Auth/ -c Release
 dotnet pack src/FreecraftCore.API.Common.Entity.Identity/ -c Release
-
+dotnet pack src/FreecraftCore.API.Common.Warden/ -c Release
---
+++ src/FreecraftCore.API.Common.Warden/WardenSubOperationCodeClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore.API.Common.Warden
+{
+	//TODO: Documentation
+	//From Trinitycore Warden.h
+	/// <summary>
+	/// Enumeration of all sub-operation codes sent by the client for Warden.
+	/// These are suboperations that only the client can send.
+	/// </summary>
+	[WireDataContract]
+	public enum WardenSubOperationCodeClient : byte
+	{
+		WARDEN_CMSG_MODULE_MISSING = 0,
+		WARDEN_CMSG_MODULE_OK = 1,
+		WARDEN_CMSG_CHEAT_CHECKS_RESULT = 2,
+
+		/// <summary>
+		/// only sent if MEM_CHECK bytes doesn't match
+		/// </summary>
+		WARDEN_CMSG_MEM_CHECKS_RESULT = 3,
+		WARDEN_CMSG_HASH_RESULT = 4,
+
+		/// <summary>
+		/// this is sent when client failed to load uploaded module due to cache fail
+		/// </summary>
+		WARDEN_CMSG_MODULE_FAILED = 5,
+	}
+}
