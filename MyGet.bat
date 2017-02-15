6f42f634d6e9c1bf35848323e9c7d3b0f52a930d
HelloKitty <andrew.blakely@ymail.com>
2017-02-14 20:19:20 -0600

    Added AddonVerificationInfo

--- MyGet.bat
+++ MyGet.bat
@@ -1,3 +1,4 @@
 dotnet pack src/FreecraftCore.API.Common.Auth/ -c Release
 dotnet pack src/FreecraftCore.API.Common.Entity.Identity/ -c Release
 dotnet pack src/FreecraftCore.API.Common.Warden/ -c Release
+dotnet pack src/FreecraftCore.API.Common.Addon/ -c Release
---
+++ src/FreecraftCore.API.Common.Addon/AddonVerificationInfo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore.API.Common
+{
+	[WireDataContract]
+	public class AddonVerificationInfo
+	{
+		//Null terminated addon name.
+		/// <summary>
+		/// The fully qualified name of the addon.
+		/// </summary>
+		[NotNull]
+		[WireMember(1)]
+		public string AddonName { get; private set; }
+
+		/// <summary>
+		/// Indicates if the addon is enabled.
+		/// </summary>
+		[WireMember(2)]
+		public bool isEnabled { get; private set; }
+
+		//TODO: How do we compute this? It looks the same for every addon in the auth packet.
+		/// <summary>
+		/// The CRC hash of the addon.
+		/// </summary>
+		[WireMember(3)]
+		private uint CRCHash { get; set; } = 1276933997; //sniffed from real client auth
+
+		//TODO: What is this?
+		/// <summary>
+		/// The something?
+		/// </summary>
+		[WireMember(4)]
+		private uint URLFileCRC { get; set; } = 0; //sniffed as 0 but I don't know what this is
+
+		public AddonVerificationInfo([NotNull] string addonName)
+		{
+			if (string.IsNullOrEmpty(addonName))
+				throw new ArgumentException("Value cannot be null or empty.", nameof(addonName));
+
+			AddonName = addonName;
+		}
+
+		protected AddonVerificationInfo()
+		{
+			//For deserialization
+		}
+	}
+}
