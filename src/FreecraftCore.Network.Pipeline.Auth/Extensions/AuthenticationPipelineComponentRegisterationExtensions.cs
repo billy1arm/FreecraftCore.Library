9e51e9da84a5c6f0e07912c39466c3bfe81a213b
HelloKitty <andrew.blakely@ymail.com>
2017-02-19 02:34:41 -0600

    Added WithPayloadDeserializationPipeline for Authentication

--- src/FreecraftCore.Network.Pipeline.Auth/Extensions/AuthenticationPipelineComponentRegisterationExtensions.cs
+++ src/FreecraftCore.Network.Pipeline.Auth/Extensions/AuthenticationPipelineComponentRegisterationExtensions.cs
@@ -6,6 +6,7 @@ using FreecraftCore.Network;
 using FreecraftCore.Packet.Auth;
 using FreecraftCore.Packet.Common;
 using FreecraftCore.Serializer;
+using JetBrains.Annotations;
 
 namespace FreecraftCore
 {
@@ -19,12 +20,30 @@ namespace FreecraftCore
 		/// </summary>
 		/// <param name="pipelineRegister">Pipeline service to register on.</param>
 		/// <returns>Register for fluent chaining.</returns>
-		public static INetworkInputPipelineRegister<IWireStreamReaderStrategyAsync, INetworkMessageContextBuilder<AuthOperationCode, IAuthenticationPacketHeader, AuthenticationPayload>, AuthOperationCode, IAuthenticationPacketHeader, AuthenticationPayload>  WithPayloadPipeline<TPipelineComponentType>(this INetworkInputPipelineRegister<IWireStreamReaderStrategyAsync, INetworkMessageContextBuilder<AuthOperationCode, IAuthenticationPacketHeader, AuthenticationPayload>, AuthOperationCode, IAuthenticationPacketHeader, AuthenticationPayload> pipelineRegister)
+		public static INetworkInputPipelineRegister<IWireStreamReaderStrategyAsync, INetworkMessageContextBuilder<AuthOperationCode, IAuthenticationPacketHeader, AuthenticationPayload>, AuthOperationCode, IAuthenticationPacketHeader, AuthenticationPayload> WithPayloadPipeline<TPipelineComponentType>(this INetworkInputPipelineRegister<IWireStreamReaderStrategyAsync, INetworkMessageContextBuilder<AuthOperationCode, IAuthenticationPacketHeader, AuthenticationPayload>, AuthOperationCode, IAuthenticationPacketHeader, AuthenticationPayload> pipelineRegister)
 			where TPipelineComponentType : IPipelineAsyncListener<IWireStreamReaderStrategyAsync, IWireStreamReaderStrategyAsync, INetworkMessageContextBuilder<AuthOperationCode, IAuthenticationPacketHeader, AuthenticationPayload>>, new()
 		{
 			//This just Activator.CreateInstance a pipeline component that doesn't require any parameters.
 			//Provides a slightly clean looking API
 			pipelineRegister.TryRegisterPipeline(new TPipelineComponentType(), NetworkPipelineTypes.Payload | NetworkPipelineTypes.Main);
+
+			//Return for fluent registeration
+			return pipelineRegister;
+		}
+
+		/// <summary>
+		/// Registers a payload deserialization pipeline in the <see cref="pipelineRegister"/> with the <see cref="NetworkPipelineTypes"/> flags NetworkPipelineTypes.Payload
+		/// </summary>
+		/// <param name="pipelineRegister">Pipeline service to register on.</param>
+		/// <param name="serializer"></param>
+		/// <returns>Register for fluent chaining.</returns>
+		public static INetworkInputPipelineRegister<IWireStreamReaderStrategyAsync, INetworkMessageContextBuilder<AuthOperationCode, IAuthenticationPacketHeader, AuthenticationPayload>, AuthOperationCode, IAuthenticationPacketHeader, AuthenticationPayload> WithPayloadDeserializationPipeline<TDeserializablePayloadType>(this INetworkInputPipelineRegister<IWireStreamReaderStrategyAsync, INetworkMessageContextBuilder<AuthOperationCode, IAuthenticationPacketHeader, AuthenticationPayload>, AuthOperationCode, IAuthenticationPacketHeader, AuthenticationPayload> pipelineRegister, [NotNull] ISerializerService serializer)
+			where TDeserializablePayloadType : AuthenticationPayload
+		{
+			if (serializer == null) throw new ArgumentNullException(nameof(serializer));
+			//This just Activator.CreateInstance a pipeline component that doesn't require any parameters.
+			//Provides a slightly clean looking API
+			pipelineRegister.TryRegisterPipeline(new PayloadReadingPipelineComponent<TDeserializablePayloadType, INetworkMessageContextBuilder<AuthOperationCode, IAuthenticationPacketHeader, AuthenticationPayload>, AuthOperationCode, IAuthenticationPacketHeader, AuthenticationPayload>(serializer), NetworkPipelineTypes.Payload | NetworkPipelineTypes.Main);
 
 			//Return for fluent registeration
 			return pipelineRegister;
