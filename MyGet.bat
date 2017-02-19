14fe7d600a7d30599ac01d47f51496695c806ca3
HelloKitty <andrew.blakely@ymail.com>
2017-02-19 03:12:11 -0600

    Prepared Myget.bat for Myget nuget building

--- MyGet.bat
+++ MyGet.bat
@@ -1,8 +1,3 @@
-dotnet pack src/FreecraftCore.Crypto.BigInteger/ -c Release
-dotnet pack src/FreecraftCore.Crypto.Common/ -c Release
-dotnet pack src/FreecraftCore.Crypto.Packet/ -c Release
-dotnet pack src/FreecraftCore.Crypto.Session/ -c Release
-dotnet pack src/FreecraftCore.Crypto.SRP6/ -c Release
-dotnet pack src/FreecraftCore.Crypto.Stream/ -c Release
-dotnet pack src/FreecraftCore.Crypto.RC4/ -c Release
-
+dotnet pack src/FreecraftCore.Network.Pipeline.API/ -c Release
+dotnet pack src/FreecraftCore.Network.Pipeline.Auth/ -c Release
+dotnet pack src/FreecraftCore.Network.Pipeline.Generic/ -c Release
