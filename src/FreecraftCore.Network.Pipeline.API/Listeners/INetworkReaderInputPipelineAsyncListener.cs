494e16ff5f23fe248c7ff8979059aa536e2ea0ea
HelloKitty <andrew.blakely@ymail.com>
2017-02-19 00:25:50 -0600

    Added Pipeline Component Interfaces

---
+++ src/FreecraftCore.Network.Pipeline.API/Listeners/INetworkReaderInputPipelineAsyncListener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreecraftCore.Packet;
+
+namespace FreecraftCore.Network
+{
+	/// <summary>
+	/// Pipeline component that can listen to network inputs.
+	/// </summary>
+	/// <typeparam name="TNetworkInputType">The network input type.</typeparam>
+	/// <typeparam name="TNetworkMessageContextBuilderType">The network context builder.</typeparam>
+	/// <typeparam name="TNetworkOperationCodeType">The operation code type.</typeparam>
+	/// <typeparam name="THeaderType">The header type.</typeparam>
+	/// <typeparam name="TPayloadType">The payload type.</typeparam>
+	public interface INetworkInputPipelineAsyncListener<TNetworkInputType, in TNetworkMessageContextBuilderType, TNetworkOperationCodeType, THeaderType, TPayloadType> : IPipelineAsyncListener<TNetworkInputType, TNetworkInputType, TNetworkMessageContextBuilderType>
+		where TNetworkMessageContextBuilderType : INetworkMessageContextBuilder<TNetworkOperationCodeType, THeaderType, TPayloadType>
+		where TNetworkOperationCodeType : struct
+		where THeaderType : IMessageVerifyable, IOperationIdentifable<TNetworkOperationCodeType>
+		where TPayloadType : IMessageVerifyable
+	{
+
+	}
+}
