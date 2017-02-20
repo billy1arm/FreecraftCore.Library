7e9dbee1b58d19a051b53746fd57a28e6ef8cd36
HelloKitty <andrew.blakely@ymail.com>
2017-02-19 20:57:05 -0600

    Added header pipeline type

---
+++ src/FreecraftCore.Network.Pipeline.API/Options/Flags/HeaderPipeline.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreecraftCore.Network
+{
+	/// <summary>
+	/// Changes the destination pipeline type to target <see cref="NetworkPipelineTypes"/>.Header.
+	/// </summary>
+	public class HeaderPipeline : IPipelineTypeFlagsStrategy
+	{
+		/// <inheritdoc />
+		public NetworkPipelineTypes TransformTypeFlags(NetworkPipelineTypes types)
+		{
+			//Transform the flags to include payloads.
+			return types | NetworkPipelineTypes.Header;
+		}
+	}
+}
