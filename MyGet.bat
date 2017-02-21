1885d599dd7fee8dfc3a0297e5754241cf14109a
HelloKitty <andrew.blakely@ymail.com>
2017-02-21 01:30:38 -0600

    Added JAMLink style NetworkConnectionContext

--- MyGet.bat
+++ MyGet.bat
@@ -1,3 +1,4 @@
 dotnet pack src/FreecraftCore.Network.Pipeline.API/ -c Release
 dotnet pack src/FreecraftCore.Network.Pipeline.Auth/ -c Release
 dotnet pack src/FreecraftCore.Network.Pipeline.Generic/ -c Release
+dotnet pack src/FreecraftCore.Network.Common/ -c Release
---
+++ src/FreecraftCore.Network.Common/Context/NetworkApplicationType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreecraftCore.Network
+{
+	/// <summary>
+	/// Enumeration of application types in the network.
+	/// </summary>
+	public enum NetworkApplicationType : int
+	{
+		/// <summary>
+		/// Indicates an unknown application type.
+		/// </summary>
+		Unknown = 0,
+
+		/// <summary>
+		/// Indicates that it represents a connection to a client.
+		/// </summary>
+		Client = 1,
+
+		/// <summary>
+		/// Generally indicates a "server" from the perspective of a client.
+		/// Actual serverside or services have a more specific denotation.
+		/// </summary>
+		Server = 2
+
+		//TODO: List all the other services. WoW says they have like 40.
+	}
+}
