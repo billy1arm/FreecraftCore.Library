0e00b8de7d8edc3754ad5f81e5fed3d3f7d6790b
HelloKitty <andrew.blakely@ymail.com>
2017-02-22 23:25:45 -0600

    Fixed MyGet.Bat

--- MyGet.bat
+++ MyGet.bat
@@ -1,3 +1,3 @@
 dotnet pack src/FreecraftCore.Handlers.Common/ -c Release
 dotnet pack src/FreecraftCore.Handlers.Auth/ -c Release
-dotnet pack src/FreecraftCore.Handlers.Client/ -c Release
+dotnet pack src/FreecraftCore.Handlers.Auth.Client/ -c Release
