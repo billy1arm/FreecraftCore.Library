6e57f2b9362b7ab939fb899f1c45dec2a6997f3d
HelloKitty <andrew.blakely@ymail.com>
2017-02-24 17:33:46 -0600

    Added RealmListResponseMessageHandler

---
+++ src/FreecraftCore.Handlers.Auth.Client/Handlers/RealmListResponseMessageHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using FreecraftCore.API.Common;
+using FreecraftCore.Network;
+using FreecraftCore.Packet.Auth;
+using JetBrains.Annotations;
+
+namespace FreecraftCore.Handlers
+{
+	public class RealmListResponseMessageHandler : AuthenticationMessageHandler<AuthRealmListResponse>
+	{
+		/// <summary>
+		/// Publishing service to push the realm information to.
+		/// </summary>
+		private IObserver<RealmInfo> OnRealmInformationAvailable { get; }
+
+		public RealmListResponseMessageHandler([NotNull] IObserver<RealmInfo> onRealmInformationAvailable)
+		{
+			if (onRealmInformationAvailable == null) throw new ArgumentNullException(nameof(onRealmInformationAvailable));
+
+			OnRealmInformationAvailable = onRealmInformationAvailable;
+		}
+
+		/// <inheritdoc />
+		protected override NetworkMessageContextState RecieveMessage(AuthenticationNetworkMessageContext context, AuthRealmListResponse stronglyTypedPayload)
+		{
+			//TODO: We will write a rules and verification engine. No need to check fields manually.
+
+			//Push all the realm info
+			stronglyTypedPayload.Realms.ToObservable().Subscribe(OnRealmInformationAvailable).Dispose();
+
+			return NetworkMessageContextState.Handled;
+		}
+	}
+}
