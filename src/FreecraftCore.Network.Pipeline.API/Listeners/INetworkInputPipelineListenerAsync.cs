f0a72cdc3dbaa67bfffdb14314e42496a9f8501e
HelloKitty <andrew.blakely@ymail.com>
2017-02-23 21:43:16 -0600

    Fixed WireWriter missing

---
+++ src/FreecraftCore.Network.Pipeline.API/Listeners/INetworkInputPipelineListenerAsync.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreecraftCore.Packet;
+
+namespace FreecraftCore.Network
+{
+	/// <summary>
+	/// Pipeline component that can listen to network inputs.
+	/// </summary>
+	/// <typeparam name="TNetworkInputType">The network input type.</typeparam>
+	/// <typeparam name="TNetworkStateType">The network context builder.</typeparam>
+	/// <typeparam name="TNetworkOperationCodeType">The operation code type.</typeparam>
+	/// <typeparam name="THeaderType">The header type.</typeparam>
+	/// <typeparam name="TPayloadType">The payload type.</typeparam>
+	public interface INetworkInputPipelineListenerAsync<TNetworkInputType, in TNetworkStateType, TNetworkOperationCodeType, THeaderType, TPayloadType> : IPipelineListenerAsync<TNetworkInputType, TNetworkInputType, TNetworkStateType>
+		where TNetworkOperationCodeType : struct
+		where THeaderType : IMessageVerifyable, IOperationIdentifable<TNetworkOperationCodeType>
+		where TPayloadType : IMessageVerifyable
+	{
+
+	}
+}
