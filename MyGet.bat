b620fbe22484e13ed0ab8478cec451ac1dbd9e74
HelloKitty <andrew.blakely@ymail.com>
2017-03-01 03:29:24 -0600

    Added Game Type Aliases for Generic Handlers

--- MyGet.bat
+++ MyGet.bat
@@ -1,3 +1,4 @@
 dotnet pack src/FreecraftCore.Handlers.Common/ -c Release
 dotnet pack src/FreecraftCore.Handlers.Auth/ -c Release
+dotnet pack src/FreecraftCore.Handlers.Game/ -c Release
 dotnet pack src/FreecraftCore.Handlers.Auth.Client/ -c Release
---
+++ src/FreecraftCore.Handlers.Game/Handlers/GameMessageHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreecraftCore.Network;
+using FreecraftCore.Packet;
+using FreecraftCore.Packet.Common;
+using JetBrains.Annotations;
+
+namespace FreecraftCore.Handlers
+{
+	/// <summary>
+	/// Base contract for all game message handlers.
+	/// </summary>
+	/// <typeparam name="TGamePayloadType">The payload this handler is responsible for.</typeparam>
+	public abstract class GameMessageHandler<TGamePayloadType> : INetworkMessagePipelineListener<GameNetworkMessageContext, NetworkOperationCode, IGamePacketHeader, GamePacketPayload>
+		where TGamePayloadType : GamePacketPayload 
+	{
+		public NetworkMessageContextState RecievePipelineMessage([NotNull] GameNetworkMessageContext input, NetworkMessageContextState currentState)
+		{
+			if (input == null) throw new ArgumentNullException(nameof(input));
+
+			TGamePayloadType stronglyTypedPayload = input.NetworkMessage.Payload as TGamePayloadType;
+			
+			//If it didn't cast or was already null then we should yield an invalid result
+			if(stronglyTypedPayload == null)
+				return NetworkMessageContextState.Invalid | currentState;
+
+			//Pass to message handling method implemented in the child class
+			return RecieveMessage(input, stronglyTypedPayload) | currentState;
+		}
+
+		/// <summary>
+		/// Message handler method. Provided context and <see cref="stronglyTypedPayload"/> and promised to never be null.
+		/// The <see cref="stronglyTypedPayload"/> is merely a strongly typed version of the payload contained in the <see cref="GameNetworkMessageContext"/> for
+		/// ease of consumption.
+		/// </summary>
+		/// <param name="context">The game message context.</param>
+		/// <param name="stronglyTypedPayload">The strongly typed casted payload.</param>
+		/// <returns></returns>
+		protected abstract NetworkMessageContextState RecieveMessage([NotNull] GameNetworkMessageContext context, [NotNull] TGamePayloadType stronglyTypedPayload);
+	}
+}
