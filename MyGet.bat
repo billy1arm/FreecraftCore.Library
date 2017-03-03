d9a60b3c5d71f454fb002178763d9747e3a5073f
HelloKitty <andrew.blakely@ymail.com>
2017-03-03 07:04:53 -0600

    Added Chat project

--- MyGet.bat
+++ MyGet.bat
@@ -3,3 +3,4 @@ dotnet pack src/FreecraftCore.API.Common.Entity.Identity/ -c Release
 dotnet pack src/FreecraftCore.API.Common.Warden/ -c Release
 dotnet pack src/FreecraftCore.API.Common.Addon/ -c Release
 dotnet pack src/FreecraftCore.API.Common.Character/ -c Release
+dotnet pack src/FreecraftCore.API.Common.Chat/ -c Release
