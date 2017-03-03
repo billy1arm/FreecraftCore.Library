38a073244c3e41d1283d85fff62ceb56d566bdcc
HelloKitty <andrew.blakely@ymail.com>
2017-03-03 09:19:44 -0600

    Added Reciever Guid to ChatMessage

--- src/FreecraftCore.API.Common.Chat/Message/GuildChatNetworkMesage.cs
+++ src/FreecraftCore.API.Common.Chat/Message/GuildChatNetworkMesage.cs
@@ -10,10 +10,16 @@ namespace FreecraftCore.API.Common
 	public class GuildChatNetworkMesage : NetworkChatMessage
 	{
 		/// <summary>
+		/// Don't really know what this would be for guild messages.
+		/// </summary>
+		[WireMember(1)]
+		public ObjectGuid RecieverGuid { get; private set; }
+
+		/// <summary>
 		/// A null terminated chat string with Length equal to the length of the text
 		/// plus one for null terminator.
 		/// </summary>
-		[WireMember(1)]
+		[WireMember(2)]
 		[SendSize(SendSizeAttribute.SizeType.Int32)] //WoW sends the size which includes the null terminator; this is likely done for efficiency
 		public string MessageText { get; private set; }
 
@@ -21,7 +27,7 @@ namespace FreecraftCore.API.Common
 		/// Indicates the current chat tag.
 		/// (Ex. DND or AFK)
 		/// </summary>
-		[WireMember(2)]
+		[WireMember(3)]
 		public ChatStateTag Tag { get; private set; }
 
 		protected GuildChatNetworkMesage()
