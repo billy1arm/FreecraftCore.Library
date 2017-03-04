95f1661abb680d768f8568adaadb7cfb9f7ff497
HelloKitty <andrew.blakely@ymail.com>
2017-03-04 06:41:43 -0600

    Added PackedGuid

--- src/FreecraftCore.API.Common.Entity.Identity/Guid/ObjectGuid.cs
+++ src/FreecraftCore.API.Common.Entity.Identity/Guid/ObjectGuid.cs
@@ -9,7 +9,7 @@ namespace FreecraftCore.API.Common
 {
 	//value-type wrapper for the object GUID from Trinitycore
 	[WireDataContract]
-	public class ObjectGuid //using a class reduces GC pressure but this object should be treated as a value-type and is immutable
+	public class ObjectGuid : BaseGuid//using a class reduces GC pressure but this object should be treated as a value-type and is immutable
 	{
 		/// <summary>
 		/// Represents an Empty or uninitialized <see cref="ObjectGuid"/>.
@@ -20,17 +20,9 @@ namespace FreecraftCore.API.Common
 		/// GUID value.
 		/// </summary>
 		[WireMember(1)]
-		public ulong RawGuidValue { get; private set; } //setter only for serialization
+		private ulong _RawGuidValue { get; set; }
 
-		/// <summary>
-		/// Indicates the object Type that the <see cref="GUID"/> is associated with.
-		/// </summary>
-		EntityGuidMask ObjectType => (EntityGuidMask)((RawGuidValue >> 48) & 0x0000FFFF);
-
-		/// <summary>
-		/// Indiciates the current GUID of the object. This is the last chunk represents the id that the world server assigned to the object. (The rest is just maskable flags about the object)
-		/// </summary>
-		public int CurrentObjectGuid => (int)(RawGuidValue & 0x0000000000FFFFFF);
+		public override ulong RawGuidValue => _RawGuidValue;
 
 		/// <summary>
 		/// Creates a new value-type wrapped for the uint64 raw GUID value.
@@ -38,31 +30,12 @@ namespace FreecraftCore.API.Common
 		/// <param name="guidValue">Raw GUID value.</param>
 		public ObjectGuid(ulong guidValue)
 		{
-			RawGuidValue = guidValue;
+			_RawGuidValue = guidValue;
 		}
 
 		protected ObjectGuid()
 		{
 			//Serializer ctor
-		}
-
-		/// <summary>
-		/// Indiciates if the GUID is an empty or unitialized GUID.
-		/// </summary>
-		/// <returns></returns>
-		public bool isEmpty()
-		{
-			return RawGuidValue == 0;
-		}
-
-		/// <summary>
-		/// Indicates if the <see cref="ObjectGuid"/> is associated with an Object Type <paramref name="guidType"/>.
-		/// </summary>
-		/// <param name="guidType">Type of GUID.</param>
-		/// <returns>True if <see cref="ObjectType"/> is the same as <paramref name="guidType"/>.</returns>
-		public bool isType(EntityGuidMask guidType)
-		{
-			return guidType == ObjectType;
 		}
 
 		/// <summary>
