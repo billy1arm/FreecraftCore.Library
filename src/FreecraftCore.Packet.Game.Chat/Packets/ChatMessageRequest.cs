c14dfe7884ea845414d50b49990da1bd2f97d66e
HelloKitty <andrew.blakely@ymail.com>
2017-03-10 21:52:05 -0600

    Added ChatMessageRequest packet

---
+++ src/FreecraftCore.Packet.Game.Chat/Packets/ChatMessageRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreecraftCore.API.Common;
+using FreecraftCore.Packet.Common;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore.Packet
+{
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_MESSAGECHAT)]
+	[ProtocolGrouping(ProtocolCode.Game)] //TODO: Change this protocol to something more specific
+	public class ChatMessageRequest : GamePacketPayload
+	{
+		/// <inheritdoc />
+		public override bool isValid { get; }
+
+		/// <summary>
+		/// The chat message.
+		/// </summary>
+		[WireMember(1)]
+		public PlayerChatMessage Message { get; private set; }
+
+		protected ChatMessageRequest([NotNull] PlayerChatMessage message)
+		{
+			if (message == null) throw new ArgumentNullException(nameof(message));
+
+			Message = message;
+		}
+	}
+}
