107d2a86526f1a92eb8b008bc7fad711c47ed1f4
HelloKitty <andrew.blakely@ymail.com>
2017-12-16 14:00:33 -0600

    Added solution with API.Common

--- merged/apicommon/Build.sh
+++
@@ -1,2 +0,0 @@
-dotnet restore
-dotnet build -c Debug src/*/project.json
--- merged/apicommon/MyGet.bat
+++
@@ -1,8 +0,0 @@
-dotnet pack src/FreecraftCore.API.Common.Auth/ -c Release
-dotnet pack src/FreecraftCore.API.Common.Entity.Identity/ -c Release
-dotnet pack src/FreecraftCore.API.Common.Warden/ -c Release
-dotnet pack src/FreecraftCore.API.Common.Addon/ -c Release
-dotnet pack src/FreecraftCore.API.Common.Character/ -c Release
-dotnet pack src/FreecraftCore.API.Common.Chat/ -c Release
-dotnet pack src/FreecraftCore.API.Common.Core/ -c Release
-dotnet pack src/FreecraftCore.API.Common.Movement/ -c Release
